a017d141ec698ec3d0dcece3bba5fe8a06c26015
Jundy25 <markjundyjuaton01@gmail.com>
2025-02-27 10:55:30 +0800

    added hover animations

--- src/App.jsx
+++ src/App.jsx
@@ -137,13 +137,17 @@ function App() {
   
   // Function to render a single task
   const renderTask = (task) => (
-    <div key={task.id} className="flex flex-col border rounded-lg py-3 px-3 gap-4"
+    <div key={task.id} className="flex flex-col border rounded-lg py-3 px-3 gap-4 hover:bg-gray-100"
       onClick={() => openModal("view", task)}>
       <div className="relative flex flex-row items-start">
         {task.completed ? (
+          // GREEN CHECK
           <svg
-            className="w-6 h-6 min-w-6 mr-1 cursor-pointer"
-            onClick={() => toggleComplete(task.id)}
+            className="w-6 h-6 min-w-6 mr-1 cursor-pointer hover:fill-green-900"
+            onClick={(e) => 
+              {
+                e.stopPropagation(); 
+                toggleComplete(task.id)}}
             viewBox="0 0 24 24"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
@@ -156,24 +160,28 @@ function App() {
             />
           </svg>
         ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-6 h-6 min-w-6 mr-1 cursor-pointer"
-            onClick={() => toggleComplete(task.id)}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M9 12.75L11.25 15L15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-            />
-          </svg>
+          // EMPTY CHECK
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-6 h-6 min-w-6 mr-1 cursor-pointer hover:fill-gray-400"
+              onClick={(e) => 
+                {
+                  e.stopPropagation(); 
+                  toggleComplete(task.id)}}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9 12.75L11.25 15L15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
         )}
         <p
-          className={`text-xs py-1 px-3 rounded-full ${
+          className={`text-xs py-1 px-3 rounded-full cursor-default ${
             task.priority === 'low'
               ? 'text-blue-500 bg-blue-50'
               : task.priority === 'medium'
@@ -184,29 +192,29 @@ function App() {
           {task.priority}
         </p>
         <div className="w-full ml-1">
-          <p className={`text-wrap font-medium flex-grow break-words ${task.completed ? "line-through" : ""}`}>{task.title}</p>
+          <p className={`text-wrap font-medium flex-grow break-words cursor-default${task.completed ? "line-through" : ""}`}>{task.title}</p>
         </div>
         <div className="flex ml-2">
-          <button onClick={() => openModal("edit", task)} className="mr-1">
+          <button onClick={(e) => {e.stopPropagation(); openModal("edit", task)}} className="mr-1 hover:cursor-pointer transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
               <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
             </svg>
           </button>
           
-          <button onClick={() => handleDeleteTask(task.id)}>
+          <button onClick={(e) => {e.stopPropagation(); handleDeleteTask(task.id)}} className="hover:cursor-pointer transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-110">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
               <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
             </svg>
           </button>
         </div>
       </div>
-      <p className="text-xs">{task.description}</p>
+      <p className="text-xs break-words cursor-default">{task.description}</p>
     </div>
   );
 
   // Function to render a time-of-day section
   const renderTimeSection = (title, tasks, icon) => (
-    <div className="flex flex-col justify-start bg-white w-full border rounded-lg py-6 px-4 gap-4 mb-6">
+    <div className="flex flex-col justify-start bg-white w-full h-fit border rounded-lg py-6 px-4 gap-4 mb-6">
       <div className="flex flex-col gap-3">
         <div className="flex flex-row h-8 space-x-2 items-center">
           {icon}
